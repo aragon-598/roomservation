2d4f35784d1ecd0e1283b339c55e3b7710a4f524
Alejandro Aragon <aragondru155@gmail.com>
2023-09-09 21:37:04 -0600

    Creating project

---
+++ src/test/java/com/room/reservation/roomservation/RoomservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.room.reservation.roomservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
